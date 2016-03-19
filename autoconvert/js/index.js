7c31baf63c122dfd8669322f3d91d7c347b077b7
jamesjackson <fiberhog@gmail.com>
2016-03-18 22:13:48 -0500

    updated

--- autoconvert/js/index.js
+++ autoconvert/js/index.js
@@ -4,8 +4,15 @@ $(document).ready(function(){
     var positions = [0, 24, 50, 65, 81, 112, 117, 121, 150, 165, 217, 228];
 
     var audioplay = document.createElement('audio');
-    audioplay.setAttribute('src', 'audio/audio123.ogg');
-    audioplay.setAttribute('src', 'audio/audio123.mp3');
+
+    if (audioplay.canPlayType('audio/mpeg')) {
+        audioplay.setAttribute('src','audio/audio123.mp3');
+    }
+
+    if (audioplay.canPlayType('audio/ogg')) {
+        audioplay.setAttribute('src','audio/audio123.ogg');
+    }
+    
     audioplay.setAttribute('preload', 'auto');
 
     var timeline = new TimelineMax({repeat:0, onStart:function() {audioplay.play()}, onComplete:function() {audioplay.pause()}});
