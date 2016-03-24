e867aaf063cb79f725647da79a01bbde7f3134b7
jamesjackson <fiberhog@gmail.com>
2016-03-24 14:21:42 -0500

    update decision tree

--- autoconvert/js/index.js
+++ autoconvert/js/index.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
     var images = $('img');
     var positions = [0, 24, 50, 65, 81, 112, 117, 121, 150, 165, 217, 229];
 
+    var pointer = images[images.length - 1];
 
     var pauseBtn = document.getElementById("pause");
 
@@ -24,9 +25,53 @@ $(document).ready(function(){
 
     timeline.totalDuration(positions[positions.length - 1]);
 
-    for (i = 0; i < images.length; i++) {
+    for (i = 0; i < images.length - 1; i++) {
         timeline.set(images[i], {css:{autoAlpha:1}}, positions[i]);
     }
+
+    //timeline.set(pointer, {css:{autoAlpha:0.5, scale:0.5}}, 2);
+
+    timeline.set(pointer, {css:{autoAlpha:0.5, scale:0.5}}, 0);
+    //timeline.to(pointer, 3, {css:{autoAlpha:0.5, scale:0.5}}, 2);
+
+    timeline.to(pointer, 2, {right:300}, 3);
+    timeline.to(pointer, 2, {bottom:100}, 5);
+
+
+    //timeline.to(pointer, 10, {
+    //
+    //    css:{autoAlpha:0.5, scale:0.5},
+    //
+    //    bezier: {
+    //        type: "soft",
+    //        values: [{
+    //            x: 0,
+    //            y: 50
+    //        }, {
+    //            x: 50,
+    //            y: 50
+    //        }, {
+    //            x: 100,
+    //            y: 50
+    //        }, {
+    //            x: 150,
+    //            y: 200
+    //        }, {
+    //            x: 50,
+    //            y: 100
+    //        }, {
+    //            x: 0,
+    //            y: 50
+    //        }]
+    //        //autoRotate: autoRotate
+    //    },
+    //    //ease for slip-n-slide-like animation wheeee
+    //    ease: Circ.easeInOut
+    //});
+
+    //timeline.to(pointer, 5, {css:{autoAlpha:0.5, scale:0.5}, bezier:{type:"cubic", values:[{x:100, y:250}, {x:150, y:100}, {x:300, y:500}, {x:500, y:400}], autoRotate:["x","y","rotation", 0, true]}, ease:Power1.easeInOut});
+
+    //timeline.to(pointer, 5, {css:{autoAlpha:0.5, scale:0.5}, bezier:[{left:100, top:250}, {left:300, top:0}, {left:500, top:400}], ease:Power1.easeInOut});
 
     pauseBtn.onclick = function() {
         timeline.paused(!timeline.paused());
